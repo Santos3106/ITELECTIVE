64c2da1001d1b66a46d8233dc83e19cb418015e9
RenzAngeloCaposo <renzangelo.caposo@tup.edu.ph>
2025-07-14 14:16:12 +0800

    Add files via upload

---
+++ register.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyACcbfm3_j2ho4NuIOCASGvNjzsrr9Ep24",
+    authDomain: "login-page-5b781.firebaseapp.com",
+    projectId: "login-page-5b781",
+    storageBucket: "login-page-5b781.firebasestorage.app",
+    messagingSenderId: "154699251140",
+    appId: "1:154699251140:web:46d3d13d69db88f94db891"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
